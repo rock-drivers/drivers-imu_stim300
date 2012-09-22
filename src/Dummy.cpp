ebbd8a96717c33559df0d175c25c7d951936f0d4
Javier Hidalgo Carrio <javier.hidalgo_carrio@dfki.de>
2012-09-22 10:38:37 +0200

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,11 @@
+#include "Dummy.hpp"
+
+namespace dummy_project
+{
+
+	void DummyClass::welcome()
+	{
+		std::cout << "You successfully compiled and executed DummyProject. Welcome!" << std::endl;
+	}
+
+}
